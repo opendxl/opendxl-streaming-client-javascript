8016d9fd6f9ad05c5fed20be7fd525879a7eccbc
k2 <kmohanasundaram_0@mcafee.com>
2022-05-31 10:06:39 -0700

    Add client id and client secret based token generation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 exports.BaseChannelAuth = require('./lib/base-channel-auth')
 exports.Channel = require('./lib/channel')
 exports.ChannelAuth = require('./lib/channel-auth')
+exports.ChannelToken = require('./lib/channel-token')
 exports.ConsumerError = require('./lib/consumer-error')
 exports.PermanentAuthenticationError = require('./lib/permanent-authentication-error')
 exports.PermanentError = require('./lib/permanent-error')
